0dbe730e81ca48311c4bf52b4c2a13ae75f80ae4
xaxoxavi <xavi.torrens@gmail.com>
2017-10-31 09:29:44 +0100

    initial

---
+++ src/main/java/servlet/FormServlet.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xavi on 30/10/17.
+ */
+public class FormServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+      String html = "<html>\n" +
+              "   <body>\n" +
+              "      <form action = \"form\" method = \"POST\">\n" +
+              "         First Name: <input type = \"text\" name = \"first_name\">\n" +
+              "         <br />\n" +
+              "         Last Name: <input type = \"text\" name = \"last_name\" />\n" +
+
+              "         <input type = \"checkbox\" name = \"maths\" checked = \"checked\" /> Maths <br/>" +
+              "         <input type = \"checkbox\" name = \"physics\"  /> Physics <br/>" +
+              "         <input type = \"checkbox\" name = \"chemistry\" checked = \"checked\" value='pollo' /> Chemistry <br/>" +
+              "         <input type = \"submit\" value = \"Submit\" />\n" +
+              "      </form>\n" +
+              "   </body>\n" +
+              "</html>";
+
+      resp.getWriter().print(html);
+      resp.setContentType("text/html");
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String firstName = req.getParameter("first_name");
+        String lastName = req.getParameter("last_name");
+
+        String chemistry = req.getParameter("chemistry");
+
+        resp.setContentType("text/html");
+        resp.getWriter().print(firstName + " " + lastName);
+        resp.getWriter().print("<br/>");
+        resp.getWriter().print(chemistry);
+
+    }
+}
---
+++ src/main/java/servlet/HelloWorld.java
@@ -0,0 +1,57 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * Created by xavi on 30/10/17.
+ */
+public class HelloWorld extends HttpServlet {
+
+    private Integer contador = null;
+
+    @Override
+    public void init() throws ServletException {
+
+        contador = new Integer(0);
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        contador += 1;
+
+        String var1Value = req.getParameter("var1");
+
+        Enumeration enumeration = req.getParameterNames();
+        while (enumeration.hasMoreElements()) {
+            String paramName = (String) enumeration.nextElement();
+            resp.getWriter().print("<h6>" + paramName + "</h6><br/>");
+
+            String[] valuesEnumeration = req.getParameterValues(paramName);
+            for (String value : valuesEnumeration){
+                resp.getWriter().print(value + "<br/>");
+            }
+
+
+        }
+
+        Cookie[] cookies = req.getCookies();
+
+        for (Cookie cookie: cookies){
+            resp.getWriter().print(cookie.getName() + " " + cookie.getValue() + "<br/>");
+        }
+
+        resp.setContentType("text/html");
+        resp.getWriter().flush();
+        resp.getWriter().close();
+
+    }
+}
